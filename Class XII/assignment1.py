71efb39100db840972b4eec153a6d7fe8d8fbe7c
AniketWithPython <98256334+AniketWithPython@users.noreply.github.com>
2022-04-04 13:27:01 +0530

    Add files via upload

--- Class XII/assignment1.py
+++ Class XII/assignment1.py
@@ -1,12 +1,12 @@
 #Assignment 1
 while True:
-    choice=input("Enter S/s for list swtich and C/c to capitalize first and last letter of each word: ")
+    choice=input("Enter S/s for list swtich and C/c to capitalize first and last letter of each word: ") #input choice
     
     if choice in 'Ss':
         #switch first index to 2nd, 2nd to 3rd and so on
         n=int(input("enter length of list: "))
         l=[]
-        for i in range(n):
+        for i in range(n):     #element inputs
             elem=int(input("enter element: "))
             l.append(elem)
         print("Before formatting:\n",l)
@@ -19,21 +19,21 @@ while True:
         str1=input("Enter string: ")
         print("Before formatting:\n",str1)
         str1=str1.lower()
-        words=str1.split()
+        words=str1.split()     #getting words of string
         nwords=[]
-        for i in words:
+        for i in words:        #capitalizing first and last letter
             m=list(i)
             m[0]=m[0].upper()
             m[-1]=m[-1].upper()
             nword=''.join(m)
             nwords.append(nword)
-        str2=' '.join(nwords)
+        str2=' '.join(nwords)  #new string
         print("After formatting:\n",str2)
 
     else:
         print("Invalid input. Try again")
         continue
 
-    yn=input("Do you want to continue?(y/n): ")
+    yn=input("Do you want to continue?(y/n): ")    #do you want to continue
     if yn in 'Nn':
         break
