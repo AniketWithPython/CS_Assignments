e507196da58532e6f48b261632e476bfe383f89e
AniketWithPython <98256334+AniketWithPython@users.noreply.github.com>
2022-04-08 10:29:32 +0530

    Add files via upload

--- Class XII/assignment2.py
+++ Class XII/assignment2.py
@@ -1,5 +1,5 @@
 #assignment 2
-def ARMSTRONG(num:int):
+def ARMSTRONG(num:int):     #to check armstrong number
     c=0
     num1=num
     while num>0:
@@ -11,7 +11,7 @@ def ARMSTRONG(num:int):
     else:
         print(num1,"is not an armstrong number")
 
-def PALIN(st:str):
+def PALIN(st:str):      #to check palindrome string
     if st==st[::-1]:
         print(st,"is a palindrome")
     else:
@@ -20,15 +20,18 @@ def PALIN(st:str):
 def main():
     while True:
         choice=input("Enter A/a for armstrong number check or P/p for palindrome string test: ")
-        if choice in "Aa":
+        if choice in "Aa":      #armstrong num check
             i=int(input("Enter number to check: "))
             ARMSTRONG(i)
-        elif choice in "Pp":
+            
+        elif choice in "Pp":    #palindrome string check
             s=input("Enter string to check: ")
             PALIN(s)
+            
         else:
             print("Invalid input. Try again")
             continue
+        
         yn=input("Do you want to continue?(y/n): ")
         if yn in "Nn":
             break
