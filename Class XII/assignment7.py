c5e57584daee39fd761b7d40a3d29b54806e1ec6
AniketWithPython <98256334+AniketWithPython@users.noreply.github.com>
2022-06-12 00:33:41 +0530

    Add files via upload

---
+++ Class XII/assignment7.py
@@ -0,0 +1,44 @@
+from tkinter import N
+
+
+def SERIES(terms:int):
+    ser=''
+    for i in range(1,terms+1):
+        ser+=', '+str((-1)**i*1/i)
+    ser=ser[2:]
+    print(ser)
+    ser='+'.join(ser.split(','))
+    print("Sum is:",eval(ser))
+
+def NEON(num:int):
+    num1=num
+    num**=2
+    sum=0
+    while num>0:
+        sum+=num%10
+        num//=10
+    if sum==num1:
+        print(num1,"Is a neon number")
+    else:
+        print(num1,"Is not a neon number")
+
+def main():
+    while True:
+        choice=input("Enter S/s for SERIES or N/n for NEON: ")
+        
+        if choice in "Ss":
+            n=int(input("Enter number of terms: "))
+            SERIES(n)
+        elif choice in "Nn":
+            n=int(input("Enter number to check: "))
+            NEON(n)
+        else:
+            print("Invalid input. Try again")
+            continue
+
+        yn=input("Do you want to continue?(y/n): ")     
+        if yn in "Nn":
+            break
+
+if __name__=="__main__":
+    main()
