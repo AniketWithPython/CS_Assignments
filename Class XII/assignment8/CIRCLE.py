bd3b3cba1424f442bf1891e4536cf702126a187a
AniketWithPython <98256334+AniketWithPython@users.noreply.github.com>
2022-06-16 21:24:43 +0530

    Add files via upload

--- Class XII/assignment8/CIRCLE.py
+++ Class XII/assignment8/CIRCLE.py
@@ -1,11 +1,11 @@
 #assignment8
 
-from math import pi 
+import math as m
 
 def ar_circle(radius):
-    ar=pi*radius**2
+    ar=m.pi*radius**2
     return ar
 
 def circum_circle(radius):
-    circum=2*pi*radius
+    circum=2*m.pi*radius
     return circum
--- Class XII/assignment8/VOL_SOLID.py
+++ Class XII/assignment8/VOL_SOLID.py
@@ -1,15 +1,15 @@
 #assignment8
 
-from math import pi
+import math as m
 
 def vol_cuboid(length,breadth,height):
     vol=length*breadth*height
     return vol
     
 def vol_cylinder(radius,height):
-    vol=height*pi*radius**2
+    vol=height*m.pi*radius**2
     return vol
 
 def vol_cone(radius,height):
-    vol=(1/3)*height*pi*radius**2
+    vol=(1/3)*height*m.pi*radius**2
     return vol
