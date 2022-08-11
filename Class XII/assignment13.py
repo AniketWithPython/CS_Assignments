ae0e8503e50201b59acdde520263bd7a8a71c5ba
AniketWithPython <98256334+AniketWithPython@users.noreply.github.com>
2022-08-11 22:54:25 +0530

    ✔

--- Class XII/assignment13.py
+++ Class XII/assignment13.py
@@ -1,28 +1,43 @@
+#assignment13
 import pickle as pkl
 def s_insert(data,fileobj):
     pkl.dump(data,fileobj)
+
 def s_disp(fileobj):
-    print("{:<12}{:<12}{:<12}{:<12}{}".format('s_no','s_name','s_rollno','s_total_marks','s_per'))
-    rec=pkl.load(fileobj)
+    print("{:<12}{:<12}{:<12}{:<14}{}".format('s_adno','s_name','s_rollno','s_total_marks','s_per'))
+    try:
+        rec=pkl.load(fileobj)
+    except EOFError:
+        print("No data")
+        return
     for r in rec:
-        print("{:<12}{:<12}{:<12}{:<12}{}".format(r[0],r[1],r[2],r[3],r[4]))
+        print("{:<12}{:<12}{:<12}{:<14}{}".format(r[0],r[1],r[2],r[3],r[4]))
+
 def s_search(search,fileobj):
     test=True
-    rec=pkl.load(fileobj)
+    try:
+        rec=pkl.load(fileobj)
+    except EOFError:
+        print("No data")
+        return
     for r in rec:
         if r[0]==search:
-            print("{:<12}{:<12}{:<12}{:<12}{}\n".format('s_no','s_name','s_rollno','s_total_marks','s_per'))
-            print("{:<12}{:<12}{:<12}{:<12}{}".format(r[0],r[1],r[2],r[3],r[4]))
+            print("{:<12}{:<12}{:<12}{:<14}{}\n".format('s_adno','s_name','s_rollno','s_total_marks','s_per'))
+            print("{:<12}{:<12}{:<12}{:<14}{}".format(r[0],r[1],r[2],r[3],r[4]))
             test=False
     if test:
         print("Sorry no records found")
+
 def s_update(data,fileobj):
-    for r in data:
+    for r in data:      #update records
         if 155<=r[3]<165:
             r[3]+=10
-    pkl.dump(data,fileobj)
+            r[4]=r[3]*0.2
+    pkl.dump(data,fileobj)      #write updated data
+    fileobj.close()
     print("Updated successfully")
     s_disp(open('./student.dat','rb'))
+
 def s_copy(fileobj):
     data=pkl.load(fileobj)
     copy=[]
@@ -33,6 +48,7 @@ def s_copy(fileobj):
         pkl.dump(copy,f)
     print("data copied succesfully")
     s_disp(open('./student_top.dat','rb'))
+
 def main():
     yn='y'
     while yn in 'yY':
@@ -41,41 +57,31 @@ def main():
             ans='y'
             dat=[]
             while ans in 'yY':
-                no=input("Enter consumer no.: ")
-                name=input("Enter consumer name: ")
-                code=input("Enter consumer area code: ")
-                phno=input("Enter consumer phoneno.: ")
-                bill=input("Enter consumer bill: ")
-                dat.append([no,name,code,phno,bill])
+                adno=input("Enter admission no.: ")
+                name=input("Enter name: ")
+                rno=input("Enter roll no.: ")
+                marks=int(input("Enter total marks(out of 500): "))
+                perc=marks*0.2
+                dat.append([adno,name,rno,marks,perc])
                 ans=input("add more?:")
-            with open('./telephone.dat','wb') as f:
-                c_insert(dat,f)
+            with open('./student.dat','wb') as f:
+                s_insert(dat,f)
         elif choice=='2':
-            with open('./telephone.dat','rb') as f:
-                c_disp(f)
+            with open('./student.dat','rb') as f:
+                s_disp(f)
         elif choice=='3':
-            f=open('./telephone.dat','rb')
-            odat=pkl.load(f)
-            f.close()
-            ans='y'
-            dat=[]
-            while ans in 'yY':
-                no=input("Enter consumer no.: ")
-                name=input("Enter consumer name: ")
-                code=input("Enter consumer area code: ")
-                phno=input("Enter consumer phoneno.: ")
-                bill=input("Enter consumer bill: ")
-                dat.append([no,name,code,phno,bill])
-                ans=input("add more?:")
-            with open('./telephone.dat','wb') as f:
-                c_append(odat,dat,f)
+            with open('./student.dat','rb') as f:
+                n=input("Enter admission no.: ")
+                s_search(n,f)
         elif choice=='4':
-            with open('./telephone.dat','rb') as f:
-                s=input("Enter consumer no.: ")
-                c_search(s,f)
+            f=open('./student.dat','rb')
+            dat=pkl.load(f)     #retrive data
+            f.close()
+            with open('./student.dat','wb') as f:
+                s_update(dat,f)
         elif choice=='5':
-            with open('./telephone.dat','rb') as f:
-                c_copy(f)
+            with open('./student.dat','rb') as f:
+                s_copy(f)
         else:
             print("Invalid input try again")
             continue
